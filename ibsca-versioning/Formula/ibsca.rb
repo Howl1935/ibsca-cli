281aa22bb2acdc934fa2a8f345111407ecc1692a
Travis Hawley <travisphawley@gmail.com>
2022-08-29 15:29:31 -0700

    Update ibsca.rb

--- ibsca-versioning/Formula/ibsca.rb
+++ ibsca-versioning/Formula/ibsca.rb
@@ -1,9 +1,9 @@
 class Ibsca < Formula
   desc "A simple CLI"
   homepage "https://github.com/Howl1935/ibsca-cli"
-  url "https://github.com/Howl1935/ibsca-cli/releases/download/v1.0.9/ibsca-macos-x64.tar.gz"
-  sha256 "f96264058922671938d166a4ec79eb7256d3144c8831cca93b294e98292a51fe"
-  version "1.0.9"  
+  url "https://github.com/Howl1935/ibsca-cli/releases/download/v1.1.0/ibsca-macos-x64.tar.gz"
+  sha256 "5fad94675c23b9946a25d9565b155e98b92e9511d2fbca0697237be74334e404"
+  version "1.1.0"  
   
   def install
     bin.install "ibsca"
